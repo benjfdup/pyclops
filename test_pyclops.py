15593047c5361a0d8ce2a95492a990809969c0ca
Ben duPont <134442292+benjfdup@users.noreply.github.com>
2025-05-28 02:15:35 +0100

    organization of tests

---
+++ test_pyclops.py
@@ -0,0 +1,8 @@
+from pyclops.core.chemical_loss_handler import ChemicalLossHandler
+
+handler = ChemicalLossHandler.from_pdb(
+    pdb_path='/Users/bendupontjr/mphil_files/all_mphil_code/tests/peptides/CDEKCG.pdb',
+    units_factor=1.0
+) 
+
+print(handler.summary())
