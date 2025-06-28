ed64e93e23a2bd82747c370a487a05421302985c
Ben duPont <134442292+benjfdup@users.noreply.github.com>
2025-06-28 22:24:15 +0100

    minor fixes

--- pyclops/utils/constants.py
+++ pyclops/utils/constants.py
@@ -6,7 +6,7 @@ KB: float = 1.0
 # AMBER caps are used to represent the N-terminus and C-terminus of a protein
 AMBER_CAPS: Set[str] = {"ACE", "NME", "NHE"}
 
-CANNONICAL_AMINO_ACID_3_LETTER_CODES: Set[str] = {
+CANONICAL_AMINO_ACID_3_LETTER_CODES: Set[str] = {
     'Ala', 'Arg', 'Asn', 'Asp', 'Cys', 'Glu', 'Gln', 
     'Gly', 'His', 'Ile', 'Leu', 'Lys', 'Met', 'Phe', 
     'Pro', 'Ser', 'Thr', 'Trp', 'Tyr', 'Val',
@@ -14,4 +14,4 @@ CANNONICAL_AMINO_ACID_3_LETTER_CODES: Set[str] = {
 
 OTHER_ALLOWED_RESIDUES: Set[str] = {} # {'ORN'} # we will support ornithine later
 
-ALLOWED_RESIDUES: Set[str] = CANNONICAL_AMINO_ACID_3_LETTER_CODES | AMBER_CAPS | OTHER_ALLOWED_RESIDUES
+ALLOWED_RESIDUES: Set[str] = CANONICAL_AMINO_ACID_3_LETTER_CODES | AMBER_CAPS | OTHER_ALLOWED_RESIDUES
